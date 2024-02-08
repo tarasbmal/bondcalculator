2aa030ae9d34d8271d9430c35f1cfb220cc064bd
tarasbmal <tarasbmal@gmail.com>
2024-02-08 15:50:35 +0300

    Update fi.py

--- fi.py
+++ fi.py
@@ -1,35 +1,90 @@
+#---------------------------------------------------------------------------------------
+#----   Функция получения SECID по параметру, который может быть  SECID,ISIN, Рег.Номеру
+#----   а также, параллельно, остальных параметров с сайта Московской биржи
+#---------------------------------------------------------------------------------------
+def micex_get_sec(p_code):
+	import pandas as pd
+	import datetime 
+	#-----   Выгружаем данные с сайта биржи в формате JSON -----
+	url = f'https://iss.moex.com/iss/engines/stock/markets/bonds/securities.json?iss.only=securities'
+	data = pd.read_json(url)
+	#---- преобразуем данные в нормальный фрейм -----
+	data = pd.DataFrame(data=data.iloc[1, 0], columns=data.iloc[0, 0])
+	#-----  Фильтруем c условием ИЛИ ----
+	res = data[(data["SECID"] == p_code) | (data["ISIN"] == p_code) | (data["REGNUMBER"] == p_code)]
+	#ret = res['SECID'].max()
+	if len(res.index)>0:
+		#-- Код бумаги 
+		sec_id = res['SECID'].iloc[0]
+		#-- Наименование 
+		sec_name = res['SECNAME'].iloc[0]
+		#-- ISIN
+		sec_isin = res['ISIN'].iloc[0]
+		#-- Дата погашения
+		mat_date = datetime.datetime.strptime(res['MATDATE'].iloc[0], "%Y-%m-%d").date()
+		#-- Дата оферты 
+		if len(str(res['OFFERDATE'].iloc[0]))>6:
+			offer_date = datetime.datetime.strptime(res['OFFERDATE'].iloc[0], "%Y-%m-%d").date()
+		else:
+			#print(len(str(res['OFFERDATE'].iloc[0])))
+			#print(res['OFFERDATE'].iloc[0])
+			offer_date = ""						
+		#-- Номинал
+		nom_sum = res['FACEVALUE'].iloc[0]
+		#-- Валюта номинала		
+		nom_cur = res['FACEUNIT'].iloc[0]
+		#-- Валюта расчетов (SUR- рубли)
+		r_cur = res['CURRENCYID'].iloc[0]
+		#-- Сумма купона
+		coup_sum = res['COUPONVALUE'].iloc[0]
+		#-----  Дата ближайшего купона
+		coup_date = datetime.datetime.strptime(res['NEXTCOUPON'].iloc[0], "%Y-%m-%d").date()
+		#-- % купона - нужно еще делить на 10000
+		coup_prc = res['COUPONPERCENT'].iloc[0]
+		#-- Период купона 
+		coup_period = res['COUPONPERIOD'].iloc[0]
+		#-- НКД (Внимание! Сумма НКД для замещающих облигаций, облигаций , номинированных в валюте - в рублях, т.е. нужно пересчитывать в валюту или просто посчитать НКД исходя из размера купона)
+		nkd = res['ACCRUEDINT'].iloc[0]
+		#-- Цена (предыдущая)
+		price = res['PREVPRICE'].iloc[0]
+		#-----------------------------------------------------------------------------
+		#-- Если скорее всего, замещающая облигация, нужно пересчитывать НКД
+		#-----------------------------------------------------------------------------
+		if res['FACEUNIT'].iloc[0] != 'SUR' and  res['CURRENCYID'].iloc[0] == 'SUR':	
+			#print("Замещающая облигация !!!")			
+			#-----  Сегодня 
+			buydate = datetime.datetime.today().date()
+			#-----  Дней до купона
+			days = (coup_date-buydate).days-1
+			#-----  Осталось НКД до купона
+			left_nkd = (res['FACEVALUE'].iloc[0] * res['COUPONPERCENT'].iloc[0] * days) / 36500
+			#----- Расчетный НКД
+			nkd = round(res['COUPONVALUE'].iloc[0] - left_nkd,2)	
+			#print(nkd)
+		#------------------------------------------------------
+		#------------------------------------------------------
+		#------------------------------------------------------
+		ret = sec_id,sec_name,sec_isin,mat_date,offer_date,nom_sum,nom_cur,r_cur,coup_sum,coup_date,coup_prc,coup_period,nkd,price
+	else:
+		ret = "","","","","",0,"","",0,"",0,0,0,0
+	return ret
+
 #----------------------------------------------------------------------
+#-------   Процедура получения данных по купонам с сайта smart-lab.ru
 #----------------------------------------------------------------------
-#-------   Процедура получения данных по облигации с сайта smart-lab.ru
-#----------------------------------------------------------------------
-#----------------------------------------------------------------------
-def get_fi_data(p_isin):
+def get_coups(p_isin):
     import pandas as pd
     import datetime 
     import requests
-    from bs4 import BeautifulSoup
-    #---- Инициализируем переменные ----
-    fi_name = ""
-    fi_mat = ""
-    fi_offer = ""
-    fi_nominal = ""
-    fi_nkd = ""
-    fi_price = ""
-    fi_couppl = ""  # Доходность купона от номинала
-    #-----------------------------------
-    #https://www.moex.com/ru/issue.aspx?board=TQCB&code=RU000A0ZYEB1&utm_source=www.moex.com/#/bond_4
     url = 'https://smart-lab.ru/q/bonds/' + p_isin
     hdr={'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
     response = requests.get(url=url,stream=True, headers=hdr, verify=False)
     st_code = response.status_code
     if st_code != 200:
-        return 0, fi_name, fi_mat, fi_offer, fi_nominal, fi_nkd, fi_price, fi_couppl, ""    #-- облигация не найдена
-    tabs = pd.read_html(response.text)
-    #--------------------------------
+        return 0, ""    #-- облигация не найдена
     #-----   Купоны -----------------
-    #--------------------------------
+    tabs = pd.read_html(response.text)
     df = tabs[0]
-    # print(df.dtypes)
     coup = df[["Дата купона","Купон"]]  #-- Оставим только нужные колонки
     coup.columns = ["cd","amounts"] #-- переименуем столбцы
     coup["dates"] = datetime.date(2000,1,2)   #-- Новый столбец даты
@@ -38,76 +93,7 @@ def get_fi_data(p_isin):
         coup['dates'][index] = datetime.datetime.strptime(coup['cd'][index], "%d-%m-%Y").date()
         coup['comment'][index] = "Купон"
     coup = coup[["dates","amounts","comment"]]    #-- оставляем только нужные поля
-    #---------------------------------------------------------
-    #-----------  Поиск параметров облигации ----------------------
-    #---------------------------------------------------------
-    soup = BeautifulSoup(response.text, "html.parser")  
-    #--- Проверка облигации с переменным купоном (такую не обрабатывем)        
-    title = soup.title # Get title Tag
-    ttl = title.string
-    if "с переменным купоном" in ttl:     
-        return -1, fi_name, fi_mat, fi_offer, fi_nominal, fi_nkd, fi_price, fi_couppl, ""    #-- облигация c переменным купоном
-    #-----  Вывод данных (для тестирования)
-    #with open("soup.txt", "w") as file:
-    #    file.write(str(soup))
-    divs = soup.find_all("div")
-    #---------  Флаги ---------------
-    flag_name = 0
-    flag_mat = 0 
-    flag_offer = 0
-    flag_nominal = 0
-    flag_nkd = 0
-    flag_price = 0
-    flag_couppl = 0
-    #--------  Цикл для извлечения параметров ----  
-    for div in divs:
-        if div.get("class") == ['quotes-simple-table__item']:
-            dtext = div.text.strip()
-            #-------------------------------------------
-            if flag_name == 1:
-                flag_name = 0
-                fi_name = dtext
-                print(fi_name)
-            if flag_mat == 1:
-                flag_mat = 0
-                fi_mat = dtext
-            if flag_offer == 1:
-                flag_offer = 0
-                fi_offer = dtext
-            if flag_nominal == 1:
-                flag_nominal = 0
-                fi_nominal = dtext
-            if flag_nkd == 1:
-                flag_nkd = 0
-                fi_nkd = dtext
-                fi_nkd = fi_nkd.split()[0]
-            if flag_price == 1:
-                flag_price = 0
-                fi_price = dtext[0:-1]  #убираем знак %
-            #if flag_couppl == 1:
-            #    flag_couppl = 0
-            #    fi_couppl = dtext[0:-1]  #убираем знак %               
-            #--------------------------------------
-            if dtext == "Имя облигации":
-                flag_name = 1
-            if dtext == "Дата погашения":
-                flag_mat = 1
-            if "Дата оферты" in dtext:
-                flag_offer = 1
-            if dtext == "Номинал":
-                flag_nominal = 1
-            if "НКД" in dtext:
-                flag_nkd = 1
-            if "Котировка облигации" in dtext:
-                flag_price = 1
-            if "Доходность купона от" in dtext:
-                flag_couppl = 1                
-            #------------------------------------
-        elif flag_couppl == 1:
-            flag_couppl = 0
-            dtext = div.text.strip()
-            fi_couppl = dtext[0:-1]  #убираем знак %   
-    return 1, fi_name, fi_mat, fi_offer, fi_nominal, fi_nkd, fi_price, fi_couppl, coup    #-- успешное завершение, облигация найдена
+    return 1, coup    #-- успешное завершение, облигация найдена
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
